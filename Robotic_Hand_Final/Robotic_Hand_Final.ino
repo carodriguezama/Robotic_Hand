edb0130eb8e948d6f85599cf66c4cd50d6cdfd61
carodriguezama <carlosrodriguezamaro5@gmail.com>
2025-04-28 17:44:15 -0600

    deleted simple servo class (useless) just need servo class)

--- Robotic_Hand_Final/Robotic_Hand_Final.ino
+++ Robotic_Hand_Final/Robotic_Hand_Final.ino
@@ -35,61 +35,41 @@ class Finger {
     }
 };
 
-class SimpleServo {
-  public:
-    int servoPin;
-    Servo servo;
-
-    SimpleServo(int pin) : servoPin(pin) {}
-
-    void attach() {
-      servo.attach(servoPin);
-    }
-
-    void setPosition(int angle) {
-      servo.write(angle);
-    }
-};
-
 Finger middleFinger(A1, 10, 850, 0.8, 0.05, 0.1, 0, 180);   // Middle & Ring (PID)
 Finger indexFinger(A3, 5, 890, 1.2, 0.05, 0.1, 180, 0);     // Index (PID)
 Finger thumbFinger(A0, 6, 800, 0.8, 0.05, 0.1, 0, 180);     // Thumb (PID)
-Finger pinkyFinger(A2, 9, 870, 0.8, 0.05, 0.1, 180, 0);     // pinky (PID)
+Finger pinkyFinger(A2, 9, 900, 0.8, 0.05, 0.1, 0, 180);     // pinky (PID)
 
-SimpleServo gripServo(3);   // Grip (simple servo)
+Servo gripServo;   // Grip (simple servo)
 
 const int buttonPin = 2;  // the number of the pushbutton pin
 int buttonState = 0;  // variable for reading the pushbutton status
 
 void setup() {
   Serial.begin(9600);
+  pinMode(buttonPin, INPUT_PULLUP);
 
   middleFinger.attach();
   indexFinger.attach();
   thumbFinger.attach();
   pinkyFinger.attach();
-  gripServo.attach();
-  
-  pinMode(buttonPin, INPUT_PULLUP);
+  gripServo.attach(3);
 
   // Startup positions
   middleFinger.servo.write(0);
   indexFinger.servo.write(180);
   thumbFinger.servo.write(180);
   pinkyFinger.servo.write(0);
-  //pinkyServo.setPosition(0);   // Open
-  gripServo.setPosition(180);  // Half-closed grip
+  gripServo.write(180);  // Half-closed grip
   delay(1500);
 
-  //pinkyServo.setPosition(90);   // Open
-  gripServo.setPosition(95);  // Half-closed grip
+  gripServo.write(95);  // Half-closed grip
   delay(1500);
 
 }
 void loop() {
   // Update PID-controlled fingers
   buttonState = digitalRead(buttonPin);
-  Serial.println(buttonState);
 
   // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
   if (buttonState == HIGH) {
@@ -98,31 +78,26 @@ void loop() {
   thumbFinger.update();
   pinkyFinger.update();
 
-
- // pinkyServo.setPosition(0);   // Set static or control dynamically
-//  gripServo.setPosition(110);  // Can be changed dynamically too
-
-  // Debugging
-  Serial.print("Middle: "); Serial.println(middleFinger.input);
-  Serial.print("Index: ");  Serial.println(indexFinger.input);
-  Serial.print("Thumb: ");  Serial.println(thumbFinger.input);
-  Serial.print("pinky: ");  Serial.println(pinkyFinger.input);
-
+Serial.print("Middle:");
+Serial.print(middleFinger.input);
+Serial.print(" Index:");
+Serial.print(indexFinger.input);
+Serial.print(" Thumb:");
+Serial.print(thumbFinger.input);
+Serial.print(" Pinky:");
+Serial.println(pinkyFinger.input);
 
   delay(500);
 
   } else {
-  // Startup positions
   middleFinger.servo.write(0);
   indexFinger.servo.write(180);
   thumbFinger.servo.write(180);
   pinkyFinger.servo.write(0);
-  //pinkyServo.setPosition(0);   // Open
-  gripServo.setPosition(180);  // Half-closed grip
+  gripServo.write(180);  // Half-closed grip
   delay(1500);
 
-  //pinkyServo.setPosition(90);   // Open
-  gripServo.setPosition(95);  // Half-closed grip
+  gripServo.write(95);  // Half-closed grip
   delay(1500);
     }
 
